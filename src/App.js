b9738993d29278860be2d12d691e158823324997
zzzAlexzzzz <hudorozhkov1985@mail.ru>
2023-07-25 11:30:01 +0500

    useContext

--- src/App.js
+++ src/App.js
@@ -6,19 +6,23 @@ import "./scss/app.scss";
 import React from "react";
 import NotFoud from "./pages/NotFoud";
 
+export const SearchContext = React.createContext("");
+
 function App() {
   const [searchValue, setSearchValue] = React.useState("");
 
   return (
     <div className="wrapper">
-      <Header searchValue={searchValue} setSearchValue={setSearchValue} />
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home searchValue={searchValue} />} />
-          <Route path="*" element={<NotFoud />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-      </div>
+      <SearchContext.Provider value={{ searchValue, setSearchValue }}>
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<NotFoud />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </div>
+      </SearchContext.Provider>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,10 @@ import PizzaBlock from "../components/PizzaBlock";
 import Sort from "../components/Sort";
 import Skeleton from "../components/pizzasSkeleton";
 import Pagination from "../components/Pagination";
+import { SearchContext } from "../App";
 
-const Home = ({ searchValue }) => {
+const Home = () => {
+  const { searchValue } = React.useContext(SearchContext);
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
   const [categoryId, setCategoryId] = React.useState(0);
